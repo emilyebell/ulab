6f7a998d1cb5e21449f8a5b4c51495ddac5a3d52
emilyebell <143663280+emilyebell@users.noreply.github.com>
2025-02-03 19:16:23 -0800

    clear out hw12 branch

--- ulab_emily/homework11/.ipynb_checkpoints/orbital-checkpoint.py
+++
@@ -1,5 +0,0 @@
-orbital.py
-
-def orbital_period(a):
-    # calculate oribital period given semi-major axis (a)
-    return (a ** 3) ** 0.5  
--- ulab_emily/homework11/kepler_law.py
+++
@@ -1,5 +0,0 @@
-kepler_law.py
-
-def orbital_period(a):
-    # calculate oribital period given semi-major axis (a)
-    return (a ** 3) ** 0.5  
